02ecfc07ece5f04b1c90df682707950e3e9de302
Sarah <sjbrooks305@gmail.com>
2020-03-19 19:04:42 -0400

    adding changes to app.py

--- app.py
+++ app.py
@@ -14,19 +14,20 @@ def handle_first_request():
     """Renders a page that shows the user the title
     of the survey, the instructions, and a button to
     start the survey. The button should serve as a link
-    that directs the user to /questions/0"""
+    that directs the user to /questions/0."""
 
-    return render_template('base.html', question_number=0)
+    return render_template('survey_home_template.html', question_number=0)
 
 @app.route('/questions/<question_number>')
 def handle_button_click(question_number):
-    """Handles the click of the start button and redirects
+    """Handles the click of the buttons and directs
     to the next question of the survey"""
 
     if question_number in locals():
         question_number = int(question_number)
     else:
         question_number = 0
+
     next_question = question_number + 1
     question_text = satisfaction_survey.questions[question_number].question
     question_choices = satisfaction_survey.questions[question_number].choices
@@ -34,4 +35,5 @@ def handle_button_click(question_number):
     return render_template('question_template.html', 
                             next_question=next_question, 
                             question_number=next_question, 
-                            question_choices=question_choices)
+                            question_choices=question_choices,
+                            question_text=question_text)
