5544fffa7a70f7c5cc4aa5fe97b9dfdff006b10a
gladkill <gladkillmusic@gmail.com>
2020-03-19 17:27:55 -0700

    boris  driving  til the wheels fall off. adjusted post request based on basic solution, as well as form on question-survey.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Question, satisfaction_survey
-
+responses = []
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fluffy'
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+
 
 @app.route('/')
 def handle_first_request():
@@ -18,12 +18,30 @@ def handle_first_request():
 
     return render_template('survey_home_template.html', question_number=0)
 
+
+@app.route('/answer', methods=["POST"])
+def collect_data():
+    selected_option = request.form['answer']
+    # print('this is our requested form info ', selected_option)
+
+    responses.append(selected_option)
+    print("!!!!!!!!THESE ARE RESPONSES \n \n ",responses)
+
+    if len(responses)==len(satisfaction_survey.questions):
+        redirect('/thankyou')
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+
+
+
 @app.route('/questions/<int:question_number>')
 def handle_button_click(question_number):
     """Handles the click of the buttons and directs
     to the next question of the survey"""
 
-    print("Printing question nubmer before if else", question_number)
+    # print("Printing question nubmer before if else", question_number)
 
     next_question = question_number + 1
     question_text = satisfaction_survey.questions[question_number].question
@@ -34,3 +52,4 @@ def handle_button_click(question_number):
                             question_number=next_question, 
                             question_choices=question_choices,
                             question_text=question_text)
+
